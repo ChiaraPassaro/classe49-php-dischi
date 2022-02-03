c812f6f3d0efd00f286e00e3b7ee09fcf5dd5356
Chiara Passaro <chiarapassaro.wd@gmail.com>
2022-02-03 12:30:54 +0100

    search

--- search.php
+++ search.php
@@ -1,14 +1,32 @@
+<?php
+include __DIR__ . '/server/db.php';
+$cardsFiltered = $cards;
+
+if (isset($_GET['genre']) !== false) {
+  $genre = $_GET['genre'];
+  if ($genre === 'all') {
+    $cardsFiltered = $cards;
+  } else {
+    $cardsFiltered = [];
+    foreach ($cards as $card) {
+      if ($card['genre'] === $genre) {
+        $cardsFiltered[] = $card;
+      }
+    }
+  }
+} ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <?php
-include __DIR__ . '/server/db.php';
 include __DIR__ . '/partials/head.php';
 ?>
 
 <body>
   <div class="container">
     <?php include __DIR__ . '/partials/header.php' ?>
-    <form action="search.php">
+    <form action="search.php" method="GET">
+      <input type="text" name="titolo">
       <select name="genre" id="genre">
         <option value="all">all</option>
         <option value="Pop">Pop</option>
@@ -19,7 +37,7 @@ include __DIR__ . '/partials/head.php';
     <main>
       <div class="cards-container">
         <?php
-        foreach ($cards as $card) {
+        foreach ($cardsFiltered as $card) {
           include __DIR__ . '/partials/card.php';
         }
         ?>
